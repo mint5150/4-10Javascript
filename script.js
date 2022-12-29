c56032e33eb433ba4226dadb35d56565c7c6d624
mint5150 <musia.home@gmail.com>
2022-12-29 11:43:18 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var Numbers = [ 3, 15, 25];
+console.log(Numbers[0]);
+console.log(Numbers[2]);
+
+var men = {
+  name: "武史",
+  age: 30,
+  hobby: "旅行",
+  }
+console.log(men.name);
+console.log(men.age);
+console.log(men.hobby);
+
+var members = [
+  {
+  name: "YUKI",
+  age: 25,
+  part: "Vocal&Guitar",
+  },
+  {
+  name: "SATOSHI",
+  age: 22,
+  subject: "Base",
+  },
+  {
+  name: "MIKI",
+  age: 32,
+  subject: "Drums",
+  }
+  ]
+console.log(members[0]);
+console.log(members[2]);
